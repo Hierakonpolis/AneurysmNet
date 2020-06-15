d40a86f126309b7d48ea73fe4b27ec4f77ed2e34
Riccardo De Feo <defeoric@puhti-login1.bullx>
2020-06-15 17:17:30 +0300

    sss
     with '#' will be ignored, and an empty message aborts the commit.

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ savebest=os.path.join(saveroot,name+'_best.pth')
 torch.set_default_tensor_type('torch.cuda.FloatTensor') # t
 torch.backends.cudnn.benchmark = True
 testsize=0.1
-Bsize=1
+Bsize=8
 workers=19
 MaxEpochs=2
 Patience=100
